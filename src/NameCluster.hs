a7af571923888b5e6bcc9fda21883cc180b38cdb
Armand (Tydax) Bour <tydaxx@gmail.com>
2016-02-29 00:18:30 +0100

    Added checkCluster(s) functions

--- src/NameCluster.hs
+++ src/NameCluster.hs
@@ -12,6 +12,8 @@
 -}
 module NameCluster (
   addToCluster,
+  checkCluster,
+  checkClusters,
   clusterify,
   distanceFromCluster,
   evaluateCentre,
@@ -33,16 +35,38 @@ addToCluster n (c:cs)
   where
     Cluster ns ct = c
 
+{-|
+  The 'checkCluster' function checks that the specified cluster is still consistent considering that the centre of
+  each cluster has been reevaluated each time that a name has been added to it.
+  The output is a list of names that were taken out of their clusters and that need to be clusterified again.
+-}
+checkCluster :: Cluster -> [Name]
+checkCluster (Cluster [] _) = []
+checkCluster c
+  | distanceFromCluster n c > formulaDistance n ct = (n:recursive)
+  | otherwise = recursive
+  where 
+    Cluster (n:ns) ct = c
+    recursive = checkCluster (Cluster ns ct)
+
+-- |The 'checkClusters' function checks that all clusters are still consistent calling the 'checkCluster' function.
+checkClusters :: [Cluster] -> [Name]
+checkClusters (c:cs) = checkCluster c ++ checkClusters cs
+
+reclusterify :: [Name] -> [Cluster] -> [Cluster]
+reclusterify [] cs = cs
+reclusterify (n:ns) cs = reclusterify ns addToCluster $ n cs
+
 -- |The 'clusterify' function distributes the specified names in clusters, using the 'addToCluster' function.
 clusterify :: [Name] -> [Cluster]
 clusterify [] = []
 clusterify (n:ns) = addToCluster n $ clusterify ns
 
 -- |The 'distanceFromCluster' function computes the distance between a Name and a Cluster.
-distanceFromCluster :: Name -> Cluster -> Int
+distanceFromCluster :: Name -> Cluster -> Distance
 distanceFromCluster name (Cluster _ ct) = distance name ct
 
--- |The 'evaluateCentre' function reevaluates the centre in the specified Cluster
+-- | TODO The 'evaluateCentre' function reevaluates the centre in the specified Cluster
 evaluateCentre :: Cluster -> Centre
 evaluateCentre (Cluster ns ct) = ct
 
